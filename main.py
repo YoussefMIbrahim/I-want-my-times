aee61c6c24b89258df0b093f9ca05fa846c7f2e3
Buster Scruggs <youssefibrahim909@gmail.com>
2024-01-31 14:06:25 -0600

    lots of testing

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 # THis is a program that will hopefully get the times from our google sheets and add them to my google calander, because I was lazy and bored
+import gspread 
 
 
 
 
 def main():
     #placeholder
+    # gc = gspread.oauth(
+    # credentials_filename='/home/busterscruggs/.config/gspread/credentials.json',
+    # authorized_user_filename='/home/busterscruggs/.config/gspread/authorized_user.json'
+    # )
+
+    gc = gspread.service_account(filename='/home/busterscruggs/.config/gspread/k')
+    sh = gc.open('Test-sheet"')
+    worksheet1 = sh.get_worksheet(0)
+
+    test1 = worksheet1.cell(1,2).value
+
+    print(test1)
     print('Hello')
+
 
 
 
