6f907c4e06807368ebe2c27ba9e6a8420d25b2b3
Buster Scruggs <youssefibrahim909@gmail.com>
2024-01-31 12:01:01 -0600

    Adding my main file

---
+++ main.py
@@ -0,0 +1,13 @@
+# THis is a program that will hopefully get the times from our google sheets and add them to my google calander, because I was lazy and bored
+
+
+
+
+def main():
+    #placeholder
+    print('Hello')
+
+
+
+
+main()
